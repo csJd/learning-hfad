a61936623d2c3fa101b6c328c647846a003dfb87
dd <ddwust@gmail.com>
2016-01-29 23:35:11 +0800

    add the interface and fragment transaction

--- Workout/app/src/main/java/com/hfad/workout/MainActivity.java
+++ Workout/app/src/main/java/com/hfad/workout/MainActivity.java
@@ -2,20 +2,27 @@ package com.hfad.workout;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.WorkoutListListener {
 
-    //private WorkoutDetailFragment fragDetail;
-    private Fragment fragList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FragmentManager fragmentManager = getFragmentManager();
+    }
 
-        //fragDetail =  (WorkoutDetailFragment)fragmentManager.findFragmentById(R.id.frag_detail);
-        fragList = fragmentManager.findFragmentById(R.id.frag_list);
+    @Override
+    public void itemClicked(long id) {
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        details.setWorkoutId((int) id);
+        ft.replace(R.id.fram_detail, details);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
     }
 }
--- Workout/app/src/main/java/com/hfad/workout/WorkoutListFragment.java
+++ Workout/app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -1,6 +1,7 @@
 package com.hfad.workout;
 
 
+import android.app.Activity;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
@@ -15,6 +17,12 @@ import android.widget.TextView;
  */
 public class WorkoutListFragment extends ListFragment {
 
+
+    public interface WorkoutListListener {
+        void itemClicked(long id);
+    }
+
+    private WorkoutListListener listener;
 
     public WorkoutListFragment() {
         // Required empty public constructor
@@ -28,5 +36,19 @@ public class WorkoutListFragment extends ListFragment {
         setListAdapter(adapter);
 
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        listener = (WorkoutListListener) activity;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if (listener != null) {
+            listener.itemClicked(id);
+        }
     }
 }
